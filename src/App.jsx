bcdf397b5325e047199b7cbd5f24ac1bfb035db8
ushant20 <ushantkumar2002@gmail.com>
2025-08-28 12:05:47 +0530

    Weathe

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+ import React from 'react'
+import Weather from './Weather App/Weather'
+ 
+ const App = () => {
+   return (
+     <div>
+      <>
+      <Weather/>
+      
+      
+      
+      </>
+       
+     </div>
+   )
+ }
+ 
+ export default App
+ 
